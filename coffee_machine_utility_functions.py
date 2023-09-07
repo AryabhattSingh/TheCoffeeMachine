0a666fe6fbd0d2c289055727fcbffd925803b095
Aryabhatt <bhattsahab314@gmail.com>
2023-09-07 20:31:25 +0530

    Added a function insert_money() to take coin input and evaluate what is the total value of the user input coins

--- coffee_machine_utility_functions.py
+++ coffee_machine_utility_functions.py
@@ -44,3 +44,37 @@ def are_resources_sufficient(coffee_choice):
 def get_coffee_cost(coffee_choice):
     """This function takes coffee name as parameter and returns the cost of the coffee"""
     return MENU[coffee_choice]['cost']
+
+
+def print_warning_message():
+    """This function prints warning message if user enters non-numeric inputs"""
+    print(f"\n{'+' * 40}")
+    print("Kindly enter integer value only.")
+    print(f"{'+' * 40}\n")
+
+
+def take_input(coin_name):
+    """This function take the name of the coin(either Quarters, Dimes, Nickels or Pennies) as a parameter and return
+     the count of the coins as input by the user"""
+    coin_value = ""
+    while not coin_value.isnumeric():
+        coin_value = input(f"How many {coin_name}? : ")
+        if not coin_value.isnumeric():
+            print_warning_message()
+    return int(coin_value)
+
+
+def insert_money():
+    """This function prompts the user to enter different types of coins, and it returns the total value of all the coins
+    inserted by the user"""
+    quarters = take_input("Quarters")
+    dimes = take_input("Dimes")
+    nickels = take_input("Nickels")
+    pennies = take_input("Pennies")
+
+    if quarters >= 0 and dimes >= 0 and nickels >= 0 and pennies >= 0:
+        return quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
+    else:
+        print(f"\n{'+' * 40}")
+        print("Kindly enter only positive value.")
+        print(f"{'+' * 40}")
